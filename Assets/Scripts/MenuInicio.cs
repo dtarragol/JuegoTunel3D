0e7bd2e42a2c4cfcdde4d306e9fd57d2cd166848
Dídac <dtarragol@gmail.com>
2023-04-20 20:45:51 +0200

    menu en lobby implementado

---
+++ Assets/Scripts/MenuInicio.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicio : MonoBehaviour
+{
+    [SerializeField] private GameObject camara;
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+        menu.SetActive(true);
+        camara.GetComponent<FirstPersonLook>().enabled = false;
+        audio.GetComponent<FirstPersonAudio>().enabled = false;
+    }
+
+    public void HideMenu()
+    {
+        Time.timeScale = 1;
+        menu.SetActive(false);
+        camara.GetComponent<FirstPersonLook>().enabled = true;
+        audio.GetComponent<FirstPersonAudio>().enabled = true;
+
+    }
+    public void JuegoMazmorra()
+    {
+        SceneManager.LoadScene("Segurridad");
+    }
+    public void JuegoTunelTerror()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
