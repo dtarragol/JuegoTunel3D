a773831ff9e414a0a846d08039fcc5d6cacfa830
marcplch <115920853+marcplch@users.noreply.github.com>
2023-05-02 18:16:15 +0200

    Plataforma movil
    arreglado script, ya funciona

--- Assets/Scripts/Scripts Marc/PlatformMovement.cs
+++ Assets/Scripts/Scripts Marc/PlatformMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformMovement : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
 	public GameObject[] waypoints;
 
